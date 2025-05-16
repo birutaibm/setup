8299e4d7b93a67b3e63c06d3864a6ed220ab1cec
Rafael Arantes <rafael.arantes@techsavvy.com.br>
2025-05-16 09:44:35 -0300

    do not disturb, Microsoft

--- dev-setup.sh
+++ dev-setup.sh
@@ -13,7 +13,7 @@ sudo apt update && sudo apt upgrade -y
 
 # Instalar dependências básicas
 echo "Instalando dependências básicas..."
-sudo apt install -y curl wget git build-essential unzip
+sudo apt install -y curl wget git build-essential unzip gpg apt-transport-https
 
 # Instalar as fontes Nerd Fonts para suportar os ícones
 echo "Instalando fontes Nerd Fonts para suportar os ícones..."
@@ -81,10 +81,11 @@ sudo systemctl enable containerd.service
 
 # Instalar VS Code
 echo "Instalando VS Code..."
-wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
-echo "code code/accepted-ms-repo boolean true" | sudo debconf-set-selections
-echo "msttcorefonts msttcorefonts/accepted-mscorefonts-eula boolean true" | sudo debconf-set-selections
-sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" -y
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
+rm -f packages.microsoft.gpg
+echo "code code/add-microsoft-repo boolean true" | sudo debconf-set-selections
 sudo apt update
 DEBIAN_FRONTEND=noninteractive sudo apt install -y code
 
